627197603de9f238b30ed28098fb8f0127a3147d
pileofwolves <nuntiswolf@gmail.com>
2023-01-14 14:35:41 +0100

    Add tooltip to ability scores to explain their purpose

---
+++ Finmer.Game/Views/Parts/AbilityScoreCharSheetView.xaml.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * FINMER - Interactive Text Adventure
+ * Copyright (C) 2019-2023 Nuntis the Wolf.
+ *
+ * Licensed under the GNU General Public License v3.0 (GPL3). See LICENSE.md for details.
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+namespace Finmer.Views
+{
+
+    /// <summary>
+    /// Interaction logic for AbilityScoreCharSheetView.xaml
+    /// </summary>
+    public partial class AbilityScoreCharSheetView
+    {
+
+        public AbilityScoreCharSheetView()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}

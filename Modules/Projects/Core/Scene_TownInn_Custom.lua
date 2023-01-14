9f50ed847a4da4ab080caf80a017a2c0e2f4fac2
pileofwolves <nuntiswolf@gmail.com>
2023-01-14 21:04:28 +0100

    Core: Remove Edgard's species randomization; he's now always a shepherd

--- Modules/Projects/Core/Scene_TownInn_Custom.lua
+++ Modules/Projects/Core/Scene_TownInn_Custom.lua
@@ -1,14 +1,13 @@
--- On first encounter, generate a random canine species for the barkeep
-if not Storage.GetFlag("barkeep_generated") then
-    Storage.SetString("barkeep_species", Text.GetString("SPECIES_CANINE"))
-    Storage.SetString("foxtaur_partner_species", Text.GetString("SPECIES_FELINE"))
-    Storage.SetFlag("barkeep_generated", true)
+-- On first encounter, generate a random species for some patrons
+if not Storage.GetFlag("TOWN_INN_PATRONS_GENERATED") then
+    Storage.SetString("TOWN_FOXTAUR_PARTNER_SPECIES", Text.GetString("SPECIES_FELINE"))
+    Storage.SetFlag("TOWN_INN_PATRONS_GENERATED", true)
 end
 
 -- Prepare text variables
 local barkeep = Creature("CR_RandomAnthro")
-barkeep.Name = Storage.GetString("barkeep_species")
-barkeep.Alias = "the " .. Storage.GetString("barkeep_species")
+barkeep.Name = "alsatian"
+barkeep.Alias = "the shepherd"
 barkeep.Gender = EGender.Male
-local ftpartner = Creature("CR_RandomAnthro")
-ftpartner.Alias = Storage.GetString("foxtaur_partner_species")
+local foxtaur_partner = Creature("CR_RandomAnthro")
+foxtaur_partner.Alias = Storage.GetString("TOWN_FOXTAUR_PARTNER_SPECIES")

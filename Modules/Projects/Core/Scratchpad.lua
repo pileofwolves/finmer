cc1c9074b75dff9b4f9a6dbe4b03eaf9c46f4a7d
pileofwolves <nuntiswolf@gmail.com>
2023-05-12 21:00:36 +0200

    Core: Update character list

--- Modules/Projects/Core/Scratchpad.lua
+++ Modules/Projects/Core/Scratchpad.lua
@@ -11,17 +11,23 @@ Iso:        pantheress, f, North Finmer mayor
 Canbell:    snow leopardess, f, North Finmer acting mayor
 Rux:        red panda, m, adept, Iso's confidant
 Gabe:       red fox, m, friendly fish merchant
-Edgard:     random canine, m, North Finmer innkeep
+Edgard:     german shepherd, m, North Finmer innkeep
 Oliver:     lion, m, forester / carpenter
+Stip:       tiger, m, Oliver's friend
+Toby:       arctic fox, m, wrong place, wrong time
 Gavin:      foxtaur, m, predator, inn regular
 Venna:      vixen, f, manages the Club
 Zosk:       chameleon, m, traveling merchant, provides mining supplies to Randall
+Chip:       feral wolf, m, Rux's guard dog, SQ02 appearance
 Pat:        stoat, m, shop owner
 Willow:     rabbit, f, shrine tender
 
     Baddies:
 Sahro:      tigress, f, MQ02 goon, shows up in MQ03
+Mako:       husky, m, unnamed in-game, MQ02 goon, shows up in MQ05
 Mia:        husky, f, replaces Sahro if she died in MQ02
+Jett:       unspecified rodent, m, unnamed in-game, RM member
+Ethel:      unspecified avian, m, unnamed in-game, RM member
 
     Mentioned:
 Wilhelm:    tabby cat, m, mentioned in werewolf short story

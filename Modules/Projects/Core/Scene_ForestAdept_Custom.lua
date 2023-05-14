bbac7f7e11f7859bdb145497dfe9b13bbb059ad4
pileofwolves <nuntiswolf@gmail.com>
2023-05-14 10:59:32 +0200

    Core: Give Rux a little shop of his own

--- Modules/Projects/Core/Scene_ForestAdept_Custom.lua
+++ Modules/Projects/Core/Scene_ForestAdept_Custom.lua
@@ -3,3 +3,30 @@
 local function IsChipVoreFatal()
     return Storage.GetNumber("MQ04_CHIP_LAST_ATE") == GetTimeHourTotal()
 end
+
+-- Shop stock
+local potions = {
+    "U_PotionHeal1",
+    "U_PotionHeal1",
+    "U_PotionDigestResist",
+}
+local function ShowRuxShop()
+    -- Basic configuration
+    local shop = Shop("ForestAdept")
+    shop.Title = "Rux"
+    shop.RestockInterval = 40
+
+    -- Restock logic
+    if shop.RestockRequired then
+        shop:MarkRestocked()
+        shop:RemoveDefaultStock()
+        shop:AddItem(Item("AC_CharmPrey"), 1)
+        shop:AddItem(Item("AC_CharmPoisonDef"), 1)
+        shop:AddItem(Item("AC_CharmArmorBreak"), 1)
+        AddRandomizableShopStock(shop, potions, 1)
+    end
+
+    -- Present shop to user
+    shop:Show()
+    shop:Save()
+end

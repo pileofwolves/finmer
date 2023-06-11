4e7778c0ef5c2c1312795e415f8ac98f3d7a507c
pileofwolves <nuntiswolf@gmail.com>
2023-06-11 16:12:57 +0200

    Add script reference for a few functions that were missing docs

---
+++ Docs/content/script-reference/world.md
@@ -0,0 +1,12 @@
+---
+sidebar_position: 3
+---
+
+import LuaReference from '@site/src/components/LuaReference';
+import ApiGroup from '@site/src/scriptref/World.json'
+
+# World & Gameplay
+
+Functions related to the world state.
+
+<LuaReference group={ApiGroup} />
--- Docs/src/components/LuaReference/index.js
+++ Docs/src/components/LuaReference/index.js
@@ -9,6 +9,18 @@
 import React from 'react';
 import styles from './styles.module.css';
 
+// Borrowed from Admonition
+function NoteIcon() {
+  return (
+    <svg viewBox="0 0 14 16">
+      <path
+        fillRule="evenodd"
+        d="M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1 3H6v5h2V4zm0 6H6v2h2v-2z"
+      />
+    </svg>
+  );
+}
+
 // Returns HTML elements describing the function signature
 function MakeArgumentList(entry) {
 	// We still want to draw the empty argument list '()' even if there are no arguments
@@ -58,6 +70,21 @@ function MakeOutputList(entry) {
 	)
 }
 
+// Returns HTML elements describing the origin of the function
+function MakeOriginNote(entry) {
+	if (!entry.coremod)
+		return null;
+
+	const text_main = "This function is part of the Core module. (Hover for details)";
+	const text_tooltip = "This function is not built-in to the game engine, but rather defined by the official Core module. If your mod setup does not load the Core module, such as with a 'total conversion' style mod, this function is not available.";
+	return (
+		<div className={styles.origin_coremod} title={text_tooltip}>
+			{NoteIcon()}
+			{text_main}
+		</div>
+	)
+}
+
 // Generates a function reference document block
 function MakeFunctionReference(entry) {
 	return (
@@ -70,6 +97,7 @@ function MakeFunctionReference(entry) {
 			{MakeInputList(entry)}
 			{MakeOutputList(entry)}
 			<div className={styles.desc}>{entry.remarks}</div>
+			{MakeOriginNote(entry)}
 		</div>
 	)
 }

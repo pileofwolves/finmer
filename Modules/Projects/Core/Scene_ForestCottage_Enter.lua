0d05499adc4a868596114ec27ce7ce8952734210
pileofwolves <nuntiswolf@gmail.com>
2023-09-02 19:03:48 +0200

    Core: Add new scenes with Oliver

--- Modules/Projects/Core/Scene_ForestCottage_Enter.lua
+++ Modules/Projects/Core/Scene_ForestCottage_Enter.lua
@@ -1,9 +1,17 @@
-if Storage.GetFlag("forest_cottage_first") then
-    Log("forest_cottage")
+-- Intro text
+if Storage.GetFlag("OLIVER_COTTAGE_AREA_FIRST") then
+    Log("OLIVER_COTTAGE_AREA")
 else
-    Storage.SetFlag("forest_cottage_first", true)
-    Log("forest_cottage_first")
+    Storage.SetFlag("OLIVER_COTTAGE_AREA_FIRST", true)
+    Log("OLIVER_COTTAGE_AREA_FIRST")
 end
 
-SetInventoryEnabled(true)
-SetLocation("Forest Edge")
+-- Improve Oliver's mood with story progression
+local mood = Storage.GetNumber("OLIVER_MOOD")
+if mood == 0 and Storage.GetFlag("MQ03_DONE") then
+    -- Enable first checkup scene after MQ03
+    Storage.SetNumber("OLIVER_MOOD", 1)
+elseif mood == 2 and GetTimeHourTotal() >= Storage.GetNumber("OLIVER_AWOL_RETURN") then
+    -- Enable main dialogue tree some time after checkup
+    Storage.SetNumber("OLIVER_MOOD", 3)
+end

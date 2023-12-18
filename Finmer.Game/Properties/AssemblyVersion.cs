1008fd4f48ce50cb7c9ab7fc943978ccf8b62bdb
pileofwolves <nuntiswolf@gmail.com>
2023-12-18 19:07:03 +0100

    Release version 1.0.1

--- Finmer.Game/Properties/AssemblyVersion.cs
+++ Finmer.Game/Properties/AssemblyVersion.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
 
 [assembly: AssemblyCompany("Nuntis the Wolf")]
 [assembly: AssemblyProduct("Finmer - Text Adventure")]
@@ -18,8 +18,8 @@ namespace Finmer
 
         public const int k_VersionMajor = 1;
         public const int k_VersionMinor = 0;
-        public const int k_VersionRevision = 0;
-        public const string k_VersionString = "1.0.0";
+        public const int k_VersionRevision = 1;
+        public const string k_VersionString = "1.0.1";
 
     }
 

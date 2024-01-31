4bac0ee46b8d19742fb3d1f16b437c976743e21a
pileofwolves <nuntiswolf@gmail.com>
2024-01-31 16:06:55 +0100

    Apparently it's 2024 now

--- Finmer.Core/VisualScripting/Nodes/CommandPreysense.cs
+++ Finmer.Core/VisualScripting/Nodes/CommandPreysense.cs
@@ -1,7 +1,7 @@
 ﻿
 /*
  * FINMER - Interactive Text Adventure
- * Copyright (C) 2019-2023 Nuntis the Wolf.
+ * Copyright (C) 2019-2024 Nuntis the Wolf.
  *
  * Licensed under the GNU General Public License v3.0 (GPL3). See LICENSE.md for details.
  * SPDX-License-Identifier: GPL-3.0-only
--- Finmer.Game/Properties/AssemblyVersion.cs
+++ Finmer.Game/Properties/AssemblyVersion.cs
@@ -5,7 +5,7 @@
 
 [assembly: AssemblyCompany("Nuntis the Wolf")]
 [assembly: AssemblyProduct("Finmer - Text Adventure")]
-[assembly: AssemblyCopyright("(C) 2019-2023. Please don't repost without permission.")]
+[assembly: AssemblyCopyright("(C) 2019-2024. Please don't repost without permission.")]
 
 namespace Finmer
 {
